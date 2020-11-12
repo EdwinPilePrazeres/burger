3f8b52fe4de0a322fe420b6d89309a9d49f48f01
Edwin Pile <69055818+EdwinPilePrazeres@users.noreply.github.com>
2020-11-11 21:52:53 -0500

    initial commit

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,9 +9,9 @@ class Modal extends Component {
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
-    componentDidUpdate() {
-        console.log('[Modal] Will Update')
-    }
+    // componentDidUpdate() {
+    //     console.log('[Modal] Will Update')
+    // }
 
     render() {
         return (
--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
+    
+
+ReactDOM.render( app, document.getElementById('root'));
 registerServiceWorker();
+
+
 
 // ReactDOM.render(
 //   <React.StrictMode>
