20c8129ca7b0eef59d3f8325061904687a8c35e1
Edwin Pile <69055818+EdwinPilePrazeres@users.noreply.github.com>
2020-12-11 22:46:42 -0500

    master

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-
+import createSagaMiddleware from 'redux-saga';
+import { watchAuth } from './store/sagas';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -20,9 +21,13 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, sagaMiddleware)
 ));
+
+sagaMiddleware.run(watchAuth);
 
 const app = (
     <Provider store={store}>
